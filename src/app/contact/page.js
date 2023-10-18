8503cb965f68aa80b7886bbf24e7bf07c8657591
suresh jangid <sureshjangir902@gmail.com>
2023-10-18 07:39:23 +0530

    update

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contect = () => {
   return (
-    <div>Contect</div>
+    <div>contact</div>
   )
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 3,
+      title: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      id: 4,
+      title: "Blog",
+      url: "/blog",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+  ];
+
+  return (
+    <div>
+      <Link href="/">Jangid Shabh</Link>
+      <div>
+      {
+      links?.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
